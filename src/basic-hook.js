e5f151db1bb425ae02f4dcabf92f01a496f0f0cc
Matt Crowder <mcrowder65@gmail.com>
2019-06-17 16:13:29 -0400

    back to original

--- src/basic-hook.js
+++ src/basic-hook.js
@@ -1,42 +1,14 @@
 import React from "react";
-import { Button } from "@material-ui/core";
-import { sleep } from "./utils";
-import { useGlobalLoader, useLoader } from "./loading";
 
-function TwoRenderProps() {
-  const global = useGlobalLoader();
-  const globalClick = () => {
-    global.makeApiCall(async () => {
-      await sleep(3000);
-    });
-  };
-  const local = useLoader();
-  const localClick = () => {
-    local.makeApiCall(async () => {
-      await sleep(1000);
-    });
-  };
+function BasicHook() {
+  const [count, setCount] = React.useState(0);
+  
   return (
-    <>
-      <Button
-        color="primary"
-        variant="contained"
-        onClick={globalClick}
-        disabled={global.isLoading}
-      >
-        Global button
-      </Button>
-
-      <Button
-        color="secondary"
-        variant="contained"
-        onClick={localClick}
-        disabled={local.isLoading}
-      >
-        Local button
-      </Button>
-    </>
+    <div>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      {count}
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
   );
 }
-
-export default TwoRenderProps;
+export default BasicHook;
