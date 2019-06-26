b86be0f9610609efa2bb2908e36466f968221558
Matt Crowder <mcrowder65@gmail.com>
2019-06-26 19:24:29 -0400

    added call to action.

--- src/conclusion.js
+++ src/conclusion.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Container } from "./reusable/container";
 import image from "./34d1vu.jpg";
+import { Button } from "./reusable/button";
+import { Text } from "./reusable/text";
 
 const Conclusion = () => {
+  const [display, setDisplay] = React.useState(false);
   return (
-    <Container>
-      <img src={image} title="made at imgflip.com" />
+    <Container flexDirection="column">
+      <Text>Let's migrate to React 16.8</Text>
+      <Button
+        style={{ width: 1, height: 1 }}
+        onClick={() => setDisplay(true)}
+      />
+      {display ? <img src={image} title="made at imgflip.com" /> : null}
     </Container>
   );
 };
